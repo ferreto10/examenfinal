b5506617e7bb51571ef98755bea65d224ae6edcf
ferreto10 <ferreto10@gmail.com>
2021-10-09 23:33:59 -0500

    Update sonar_scanner.sh

--- sonar_scanner.sh
+++ sonar_scanner.sh
@@ -1,11 +1,10 @@
 echo  Installing Sonar Scanner CLI
-wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
-unzip sonar-scanner-cli-4.6.2.2472-linux.zip
-export PATH=$(pwd)sonar-scanner-cli-4.6.2.2472-linux/bin:$PATH
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip  
+unzip sonar-scanner-cli-3.3.0.1492-linux.zip -d /tmp
 echo Sonar Scanner Installed!
 echo Running Sonar Scanner...
 #Agregar el Comando de Sonar  Scanner
-sonar-scanner \
+/tmp/sonar-scanner-3.3.0.1492-linux/bin/sonar-scanner \
   -Dsonar.projectKey=EXF_Mario_FerretoR \
   -Dsonar.sources=. \
   -Dsonar.host.url=https://sonar.semanticsecurity.org \
